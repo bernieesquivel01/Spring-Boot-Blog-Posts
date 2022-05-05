23461ae4dabc1e000a93cb6e84b141c3a182fb63
Bernie Esquivel <bernie.esquivel01@gmail.com>
2022-05-04 21:48:29 -0500

    spring boot blog posts

---
+++ src/test/java/com/example/springbootblogposts/SpringBootBlogPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootblogposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogPostsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
